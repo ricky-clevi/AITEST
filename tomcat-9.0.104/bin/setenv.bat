0cea329921f86f51aa3f6b77e7ae6c850a988792
Ricky <ricky@clevi.ai>
2026-01-09 10:25:34 +0900

    removed things that are not needed in the gitignore

--- tomcat-9.0.104/bin/setenv.bat
+++ tomcat-9.0.104/bin/setenv.bat
@@ -4,7 +4,7 @@ rem Set environment variables for Tomcat startup
 rem ---------------------------------------------------------------------------
 
 rem Set JVM options for proper encoding
-set JAVA_OPTS=%JAVA_OPTS% -Dfile.encoding=MS949 -Dconsole.encoding=MS949 -Duser.language=ko -Duser.country=KR -Duser.timezone=Asia/Seoul -Dsun.stdout.encoding=MS949 -Dsun.stderr.encoding=MS949 -javaagent:%CATALINA_HOME%\springloaded-1.2.8.RELEASE.jar -noverify -Dspringloaded.debug=true -Dspringloaded.logLevel=DEBUG -Dproperty-override=%CATALINA_HOME%\webapps\mycompany\WEB-INF\classes\hibernate-fix.properties
+set JAVA_OPTS=%JAVA_OPTS% -Dfile.encoding=MS949 -Dconsole.encoding=MS949 -Duser.language=ko -Duser.country=KR -Duser.timezone=Asia/Seoul -Dsun.stdout.encoding=MS949 -Dsun.stderr.encoding=MS949 -javaagent:%CATALINA_HOME%\springloaded-1.2.8.RELEASE.jar -noverify -Dspringloaded.debug=true -Dspringloaded.logLevel=DEBUG -Dproperty-override=%CATALINA_HOME%\webapps\mycompany\WEB-INF\classes\hibernate-fix.properties -Ddatabase.user=sa -Ddatabase.password= -Ddatabase.driver=org.hsqldb.jdbcDriver -Ddatabase.url=jdbc:hsqldb:hsql://localhost:9001/broadleaf -Druntime.environment=development
 
 rem Set CATALINA_OPTS for additional Tomcat specific options
 set CATALINA_OPTS=%CATALINA_OPTS%
