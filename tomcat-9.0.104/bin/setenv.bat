355bc5e7e957e4d675d0af82c5949389544316b7
Ricky <ricky@clevi.ai>
2026-01-19 19:31:48 +0900

    Clean runtime artifacts and simplify local runners

--- tomcat-9.0.104/bin/setenv.bat
+++ tomcat-9.0.104/bin/setenv.bat
@@ -7,7 +7,7 @@ rem Set JAVA_HOME to JDK 8
 set JAVA_HOME=C:\Program Files\Java\jdk1.8.0_202
 
 rem Set JVM options for proper encoding
-set JAVA_OPTS=%JAVA_OPTS% -Dfile.encoding=MS949 -Dconsole.encoding=MS949 -Duser.language=ko -Duser.country=KR -Duser.timezone=Asia/Seoul -Dsun.stdout.encoding=MS949 -Dsun.stderr.encoding=MS949 -javaagent:%CATALINA_HOME%\springloaded-1.2.8.RELEASE.jar -noverify -Dspringloaded.debug=true -Dspringloaded.logLevel=DEBUG -Dproperty-override=%CATALINA_HOME%\webapps\hibernate-fix-combined.properties
+set JAVA_OPTS=%JAVA_OPTS% -Dfile.encoding=MS949 -Dconsole.encoding=MS949 -Duser.language=ko -Duser.country=KR -Duser.timezone=Asia/Seoul -Dsun.stdout.encoding=MS949 -Dsun.stderr.encoding=MS949 -javaagent:%CATALINA_HOME%\springloaded-1.2.8.RELEASE.jar -noverify -Dspringloaded.debug=true -Dspringloaded.logLevel=DEBUG -Dproperty-override=%CATALINA_HOME%\webapps\hibernate-fix-combined.properties -Ddatabase.user=sa -Ddatabase.password= -Ddatabase.driver=org.hsqldb.jdbcDriver -Ddatabase.url=jdbc:hsqldb:hsql://localhost:9001/broadleaf -Druntime.environment=development
 
 rem Set CATALINA_OPTS for additional Tomcat specific options
 set CATALINA_OPTS=%CATALINA_OPTS%
